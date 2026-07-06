dc08ccea483e71e2310e8fbf588a5de9ee9808e3
수경 <sky339128@gmail.com>
2026-07-06 23:53:56 +0900

    feat: Vercel Analytics 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Analytics } from '@vercel/analytics/react';
 import AppRouter from './routes';
 
 export default function App() {
-  return <AppRouter />;
+  return (
+    <>
+      <AppRouter />
+      <Analytics />
+    </>
+  );
 }
